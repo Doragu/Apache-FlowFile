bb21ad0a8a097c8fa7a2386e9648d80da96e3395
Krzysztof Chybicki <krysiek97@interia.eu>
2019-05-21 20:24:15 +0200

    Cleaner Python, better synchronization

--- kafka/inputjson-topic.py
+++ kafka/inputjson-topic.py
@@ -5,32 +5,52 @@ from json import dumps
 from time import sleep
 from kafka import KafkaProducer
 
-sleep(30)
 
-path = "./input_json"
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
+def connector():    
+    result = False
 
-before = dict ([(f, None) for f in os.listdir (path)])
+    while result is False:
+        try:
+            producer = KafkaProducer(bootstrap_servers=['kafka:29092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
+            result = True   
+        except:
+            pass
+        finally:
+            sleep(0.5)
 
-for file in before:
-    if ".json" in file:
-        with open(path+'/'+file) as f:
+    return producer
+
+def sendToKafka(file):
+    with open(path+'/'+file) as f:
+        try:
             data = json.load(f)
+        except:
+            pass
 
-        for line in data:
-            producer.send('json', value=line)
-            sleep(0.1)
+    for line in data:
+        producer.send('json', value=line)
 
-while 1:
-    sleep (5)
-    after = dict ([(f, None) for f in os.listdir (path)])
-    added = [f for f in after if not f in before]
-    if added:
-        for file in added:
-            if ".json" in file:
-                with open(path+'/'+file) as f:
-                    data = json.load(f)
-                for line in data:
-                    producer.send('json', value=line)
-                    sleep(0.1)
-    before = after
+def checkForNewFiles(before):
+    while 1:
+        sleep (5)
+        after = dict ([(f, None) for f in os.listdir (path)])
+        added = [f for f in after if not f in before]
+        if added:
+            for file in added:
+                if ".json" in file:
+                    sendToKafka(file)
+        before = after
+
+if __name__ == "__main__":
+    producer = connector()
+
+    print("Connection passed")
+
+    path = "./input_json"
+    before = dict ([(f, None) for f in os.listdir (path)])
+
+    for file in before:
+        if ".json" in file:
+            sendToKafka(file)
+
+    checkForNewFiles(before)
